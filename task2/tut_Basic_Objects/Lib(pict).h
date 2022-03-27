a1761f4ee08b1926bf742a1a2864b2f442e2e14e
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-03-27 16:15:27 +0300

    Add files via upload

---
+++ task2/tut_Basic_Objects/Lib(pict).h
@@ -0,0 +1,12 @@
+#pragma once
+#include "GL/freeglut.h"
+#define _USE_MATH_DEFINES 
+#include <cmath>
+
+void processKeys(unsigned char key, int x, int y);
+void drawKosa(float x, float y, float a);
+void drawCSENA();
+void drawExcit(float x, float y);
+void drawViktory();
+void drawWalls();
+void drawRat(float x, float y);
