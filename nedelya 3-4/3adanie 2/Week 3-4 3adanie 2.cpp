94adca213528fde8b08a97335e4fa4284cd2fe70
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 14:25:55 +0300

    Add files via upload

---
+++ nedelya 3-4/3adanie 2/Week 3-4 3adanie 2.cpp
@@ -0,0 +1,18 @@
+﻿//Протабулировать функцию (шаг и диапазон задаёт пользователь):
+//y = sqrt(-(1 + 3x) / 2) + 3x;
+
+#include <iostream>
+
+int main()
+{
+    float Shag, DiapNachal, DiapKonec;
+    std::cout << "Enter shag i diapazon\n";
+    std::cin >> Shag >> DiapNachal >> DiapKonec;
+    for (DiapNachal; DiapNachal <= DiapKonec; DiapNachal = DiapNachal + Shag)
+    {
+        float y, x;
+        x = DiapNachal;
+        y = -sqrt((1 + 3 * x) / 2) + 3 * x;
+        std::cout << x << "\t" << y << "\n";
+    }
+}
