6bf3dc123cbb3cae3a9da0ccc2e82ba9a02cac60
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 19:20:48 +0300

    Add files via upload

---
+++ nedelya 13-14/3adanie 5/Week 13-14 3adanie 5.cpp
@@ -0,0 +1,27 @@
+﻿//11. Вывести элементы массива, меньших среднего арифметического
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+void Mass() {
+    float SrArifm = 0;
+    array<int, 25>Massiv;
+    for (int i = 0; i < 25; i++) {
+        Massiv[i] = rand() % 50 + 1;
+        SrArifm += Massiv[i];
+        cout << Massiv[i] << endl;
+    }
+    SrArifm = SrArifm / 25;
+    cout << SrArifm << endl;
+
+    for (int i = 0; i < 25; i++) {
+        if (Massiv[i] < SrArifm)
+            cout << "Less than SrArifm:" << Massiv[i] << "\n";
+    }
+}
+
+int main()
+{
+    Mass();
+}
