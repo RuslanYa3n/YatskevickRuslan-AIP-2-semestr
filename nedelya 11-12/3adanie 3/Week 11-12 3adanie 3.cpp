c0ff77388f64362643ca6d391f885c145975a52e
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 16:54:58 +0300

    Add files via upload

---
+++ nedelya 11-12/3adanie 3/Week 11-12 3adanie 3.cpp
@@ -0,0 +1,32 @@
+﻿// Имеется информация о датчиках : диапазон измеряемых значений(min, max),
+//погрешность, максимальная скорость.Вывести данные о датчике с наибольшим
+//диапазоном значений.
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+struct Datchiki {
+    int min, max;
+    float Pogreshnost, MaxSpeed, diap;
+};
+
+Datchiki datch[5];
+
+int main()
+{
+    int tochn;
+    float DiapazonMax = 0;
+    for (int i = 0; i < 5; i++) {
+        datch[i].max = 100 + rand() % 200;
+        datch[i].min = 1 + rand() % 20;
+        datch[i].diap = datch[i].max + datch[i].min;
+
+        if (datch[i].diap > DiapazonMax)
+        {
+            tochn = i;
+            DiapazonMax = datch[i].diap;
+        }
+    }
+
