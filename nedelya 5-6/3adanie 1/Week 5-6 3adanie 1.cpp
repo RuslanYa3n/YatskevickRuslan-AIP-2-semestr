d1fe92f9380190274764017446c79c459ad2e40c
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 15:24:44 +0300

    Add files via upload

---
+++ nedelya 5-6/3adanie 1/Week 5-6 3adanie 1.cpp
@@ -0,0 +1,19 @@
+﻿//   y = 2 * | x + 6 | -x^2, åñëè x < 5
+
+#include <iostream>
+
+int main()
+{
+    float x;
+    std::cin >> x;
+
+    if (x > 5)
+        std::cout << 2 * (x * x) + 10;
+    else if (x == 0)
+
+        std::cout << 0 * x;
+    else if (x < 5)
+        std::cout << 2 * fabs(x + 6) - (x * x);
+
+
+}
