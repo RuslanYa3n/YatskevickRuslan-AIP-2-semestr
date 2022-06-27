7beff86ff4b57a2511428fbbb92785bf2f802b3f
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 14:31:25 +0300

    Add files via upload

---
+++ nedelya 3-4/3adanie 3/Week 3-4 3adanie 3.cpp
@@ -0,0 +1,18 @@
+﻿//Протабулировать функцию (шаг и диапазон задаёт пользователь)
+//y=sqrt(5+4*x)+13*x+cos(12*x+91)
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float Shag, DiapNachal, DiapKonec;
+    std::cout << "Enter shag i diapazon\n";
+    std::cin >> Shag >> DiapNachal >> DiapKonec;
+    for (DiapNachal; DiapNachal <= DiapKonec; DiapNachal = DiapNachal + Shag)
+    {
+        float y, x;
+        x = DiapNachal;
+        y = sqrt(5 + 4 * x) + 13 * x + cos(12 * x + 91);
+        std::cout << x << "\t" << y << "\n";
+    }
+}
