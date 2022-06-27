6ee9fa14c3b74cd4b74ee8056896e92a50ac7676
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 13:13:26 +0300

    Add files via upload

---
+++ nedelya 1-2/3adanie 3/3adanie 3.cpp
@@ -0,0 +1,11 @@
+﻿//3.Пользователь вводит два числа. Необходимо поменять значения переменных так, чтобы значение первой оказалось во второй, а второй в первой.
+
+#include <iostream>
+int main() {
+    float Num1, Num2;
+    std::cout << "Enter two numbers\n";
+    std::cin >> Num1 >> Num2;
+    std::swap(Num1, Num2);
+    std::cout << "Num1: " << Num1 << "\t" << "Num2: " << Num2;
+    std::getchar();
+}
