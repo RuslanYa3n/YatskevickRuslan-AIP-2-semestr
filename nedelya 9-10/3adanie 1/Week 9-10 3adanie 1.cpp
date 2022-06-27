98febb1ef7acf75dc36f5d59aed1740a2862ea61
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 15:43:06 +0300

    Add files via upload

---
+++ nedelya 9-10/3adanie 1/Week 9-10 3adanie 1.cpp
@@ -0,0 +1,20 @@
+﻿//Рефакторинг кода
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void Mean(float x, float y, float& AMean, float& GMean) {
+
+    AMean = (x + y) / 2;
+    GMean = sqrt(x * y);
+}
+int main()
+{
+    float s, e, A, G;
+    s = 3;
+    e = 1;
+    Mean(s, e, A, G);
+    cout << "Itog Amean: " << A << std::endl;
+    cout << "Itogv Gmean: " << G << std::endl;
+    return 0;
+}
