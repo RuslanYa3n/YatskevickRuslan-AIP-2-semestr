66d3225c7b559ca0ed44fd64028a64a96f9d9dde
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 13:30:36 +0300

    Add files via upload

---
+++ nedelya 1-2/3adanie 5/3adanie 5.cpp
@@ -0,0 +1,39 @@
+﻿//  Конвертер велечин(паскаль, бар, фунт на квадратный дюйм)
+
+#include <iostream>
+
+int main()
+{
+    int I;
+    float x;
+    std::cout << "Направление перевода" << "\n" << "1-Паскаль в бар" << "\n" << "2-Паскаль в фунт" << "\n" << "3-Бар в паскаль" << "\n" << "4-Бар в фунт" << "\n" << "5-фунт в паскаль" << "\n" << "6-Фунт в бар" << std::endl;
+    std::cin >> I;
+    std::cout << "Направление перевода" << std::endl;
+    std::cin >> x;
+    if (I == 1) {
+        x = x * 0.00001;
+        std::cout << x << " Bar";
+    };
+    if (I == 2) {
+        x = x * 0.000145;
+        std::cout << x << " Funt na kvadratniy duym";
+    };
+    if (I == 3) {
+        x = x * 100000;
+        std::cout << x << " Paskal";
+    };
+    if (I == 4) {
+        x = x * 14.5;
+        std::cout << x << " Funt na kvadratniy duym";
+    };
+    if (I == 5) {
+        x = x * 6894.76;
+        std::cout << x << " Paskal";
+    };
+    if (I == 6) {
+        x = x * 0.069;
+        std::cout << x << " Bar";
+    };
+
+
+
