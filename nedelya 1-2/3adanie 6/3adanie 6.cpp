f2c6a8f9742adbb1bb0b952fe30113db05844712
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 13:36:06 +0300

    Add files via upload

---
+++ nedelya 1-2/3adanie 6/3adanie 6.cpp
@@ -0,0 +1,42 @@
+﻿// Сделать калькулятор. Формат ввода: Операция Ч1 Ч2
+
+#include <iostream>
+
+int main()
+{
+    float x1, x2;
+    char Operation;
+    std::cout << "Choose operation and 2 numbers" << std::endl;
+
+    std::cin >> Operation >> x1 >> x2;
+    if (Operation == '/') {
+        std::cout << x1 / x2;
+    }
+    if (Operation == '*') {
+        std::cout << x1 * x2;
+    }
+    if (Operation == '-') {
+        std::cout << x1 - x2;
+    }
+    if (Operation == '+') {
+        std::cout << x1 + x2;
+    }
+
+
+
+    switch (Operation)
+    {
+    case '+':
+        std::cout << x1 + x2;
+        break;
+    case '-':
+        std::cout << x1 - x2;
+        break;
+    case '*':
+        std::cout << x1 * x2;
+        break;
+    case '/':
+        std::cout << x1 / x2;
+        break;
+    }
+}
