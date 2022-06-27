1f8ad8fe2e49a5a7644f7ca512cd31daca9a3818
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 17:20:09 +0300

    Add files via upload

---
+++ nedelya 11-12/3adanie 5/Week 11-12 3adanie 5.cpp
@@ -0,0 +1,31 @@
+﻿//11. Робот-автомобиль описывается как структура с полями: скорость, длительность
+//движения и номер.Напишите функцию для расчёта пройденного расстояния.
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+
+struct Car {
+    float speed, way;
+    int time, number;
+};
+
+Car cars[3];
+
+int main()
+{
+    for (int i = 0; i < 3; i++) {
+        {
+            cars[i].time = 1 + rand() % 36;
+            cars[i].speed = 1 + rand() % 280;
+            cars[i].number = i;
+            cars[i].way = cars[i].speed * cars[i].time;
+            std::cout << "\nWay car " << i << ":\t" << cars[i].way;
+        }
+    }
+
+
+
+}
