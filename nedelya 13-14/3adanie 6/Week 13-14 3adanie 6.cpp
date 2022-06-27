4bba49b4735d8b8e72cdc601f9315e1d82ec8ece
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 19:23:33 +0300

    Add files via upload

---
+++ nedelya 13-14/3adanie 6/Week 13-14 3adanie 6.cpp
@@ -0,0 +1,26 @@
+﻿//14. Создать новый массив из двух других массивов путём операций над элементами массивов вычитания
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+void Massiv() {
+	array<int, 5>Massiv1;
+	array<int, 5>Massiv2;
+	array<int, 5>MassivKonec;
+	for (int i = 0; i < 5; i++) {
+		Massiv1[i] = rand() % 35;
+		Massiv2[i] = rand() % 15;
+		MassivKonec[i] = Massiv1[i] - Massiv2[i];
+		cout << "Num1: " << Massiv1[i] << ";  Num2: " << Massiv2[i] << ";  NumKonec: " << MassivKonec[i] << endl;
+	}
+
+}
+
+
+
+int main()
+{
+	Massiv();
+}
