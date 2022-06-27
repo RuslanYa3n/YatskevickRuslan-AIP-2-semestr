f71c68e8a1af0b2cdc1d44414f3354cfe0631797
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 19:15:03 +0300

    Add files via upload

---
+++ nedelya 13-14/3adanie 3/Week 113-14 3adanie 3.cpp
@@ -0,0 +1,25 @@
+﻿// Посчитать количество положительных/отрицательных элементов массива
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+void Elements() {
+    int Pol = 0, Otric = 0;
+    array<int, 25>Element;
+    for (int i = 0; i < 25; i++)
+    {
+        Element[i] = rand() % 50 - 25;
+        if (Element[i] >= 0)
+            Pol++;
+        else Otric++;
+        cout << Element[i] << "\n";
+    }
+    cout << "Polojit: " << Pol << "\nOtricat: " << Otric;
+}
+
+int main()
+{
+    Elements();
+}
