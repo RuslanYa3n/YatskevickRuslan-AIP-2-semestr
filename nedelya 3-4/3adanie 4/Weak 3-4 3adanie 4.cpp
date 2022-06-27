30af6aa39e91d84daf2be08c1ae9746ca8517558
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 14:35:31 +0300

    Add files via upload

---
+++ nedelya 3-4/3adanie 4/Weak 3-4 3adanie 4.cpp
@@ -0,0 +1,22 @@
+﻿
+//Пользователь задаёт число Ч и Х. Посчитать y:
+//y=x-2*x+3*x-4*x+...+Chislo*x;
+
+#include <iostream>
+
+int main()
+{
+    float x, y = 0, Num;
+    std::cout << "Enter Number i X\n";
+    std::cin >> Num >> x;
+    for (int i = 1; i <= Num; i = i + 2)
+    {
+        y = y + i * x;
+    }
+    for (int i = 2; i <= Num; i = i + 2)
+    {
+        y = y - i * x;
+    }
+
+    std::cout << y;
+}
