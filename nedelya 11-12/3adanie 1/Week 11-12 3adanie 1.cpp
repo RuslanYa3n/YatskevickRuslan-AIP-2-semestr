065cda02ac6affaac0a6b9e7da69cd702bd2c520
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 16:48:47 +0300

    Add files via upload

---
+++ nedelya 11-12/3adanie 1/Week 11-12 3adanie 1.cpp
@@ -0,0 +1,27 @@
+﻿//1. Создайте структуру, которая хранит время: часы, минуты, секунды
+//b. Написать функцию, которая сравнивает два времени. Возвращает true, если
+//первый аргумент больше второго.
+
+#include <iostream>
+#include <array>
+
+struct Time {
+    int hours; int minutes; int seconds;
+};
+
+Time times[2];
+
+int main()
+{
+    times[1].hours = 1 + rand() % 60;
+    times[2].hours = 1 + rand() % 60;
+    times[1].minutes = 1 + rand() % 60;
+    times[2].minutes = 1 + rand() % 60;
+    times[1].seconds = 1 + rand() % 60;
+    times[2].seconds = 1 + rand() % 60;
+    if (times[1].hours > times[2].hours)
+        return true;
+    if (times[1].hours < times[2].hours)
+        return false;
+
+}
