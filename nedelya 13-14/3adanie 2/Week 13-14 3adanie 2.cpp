be6487e836e52fe9cf397e4107740abf1d7500f9
RuslanYa3n <91945038+RuslanYa3n@users.noreply.github.com>
2022-06-27 19:12:10 +0300

    Add files via upload

---
+++ nedelya 13-14/3adanie 2/Week 13-14 3adanie 2.cpp
@@ -0,0 +1,27 @@
+﻿//Вычислить сумму элементов массива:
+//меньше 5, больше 6, меньше 4
+
+#include <iostream>
+#include <array>
+
+
+void Summ() {
+    int Summa5 = 0, Summa6 = 0, Summa4 = 0;
+    std::array<int, 20> Summa;
+    for (int i = 0; i < 20; i++) {
+        Summa[i] = rand() % 15;
+        if (Summa[i] < 5)
+            Summa5 += Summa[i];
+        if (Summa[i] > 6)
+            Summa6 += Summa[i];
+        if (Summa[i] < 4)
+            Summa4 += Summa[i];
+    }
+    std::cout << "\nSumma <5  " << Summa5 << "\nSumma >6  " << Summa6 << "\nSumma <4  " << Summa4;
+}
+
+
+int main()
+{
+    Summ();
+}
